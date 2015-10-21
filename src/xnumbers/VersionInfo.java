98c43b02578af4d9c54e3a12694c304c830aacb6
ganast <anastas@unipi.gr>
2015-10-21 03:14:53 +0300

    Added VersionInfo.

---
+++ src/xnumbers/VersionInfo.java
@@ -0,0 +1,17 @@
+package xnumbers;
+
+/**
+ * Automatically-generated version information.
+ */
+public final class VersionInfo {
+	public static final String PROJECT_NAME = "XNumbers";
+	public static final String PROJECT_AUTHOR = "George Anastassakis";
+	public static final String PROJECT_DESCRIPTION = "An implementation of the N-Puzzle game in X3D";
+	public static final String PROJECT_HOMEPAGE = "http://kelnet.cs.unipi.gr/ganast/index.php?p=x3d";
+	public static final int VERSION_MAJOR = 2;
+	public static final int VERSION_MINOR = 0;
+	public static final int VERSION_IMPLEMENTATION = 2;
+	public static final int VERSION_BUILD = 38;
+	public static final String VERSION_TSTAMP = "201004270052";
+	public static final String VERSION_DATE = "April 27 2010";
+}
